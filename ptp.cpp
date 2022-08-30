06ada16886bd781921350f654db7193ee084e1b3
mahdimoeini8102 <mahdimoeini8102@gmail.com>
2022-08-30 16:05:11 +0430

    delay_resp_ptp added

--- ptp.cpp
+++ ptp.cpp
@@ -36,7 +36,7 @@ void generate_ptp_header (char type, unsigned char* result) {
         string_bintohex((unsigned char*) b_messageLength, s_messageLength);
         strcopy(&s_messageLength[0], messageLength, 0, 3);
     }
-    else if(type == ptp_type::pdelay_req_ptp || type == ptp_type::pdelay_resp_ptp){
+    else if(type == ptp_type::pdelay_req_ptp || type == ptp_type::pdelay_resp_ptp || type == ptp_type::delay_resp_ptp){
         strcpy(b_messageLength, "0000000000110110\0");
         string_bintohex((unsigned char*) b_messageLength, s_messageLength);
         strcopy(&s_messageLength[0], messageLength, 0, 3);
@@ -276,6 +276,39 @@ void ptp_send (EthernetSocket ethSocket, char* init_packet) {
             strcat(packet.string_bin, preciseOriginTimestamp.string_bin);
             packet.size_bin += preciseOriginTimestamp.size_bin;
 
+
+            packet_to_buf(packet.buf, packet.string_bin);
+            packet.size_buf = packet.size_bin / 8;
+
+            bytes_sended = Ethernet_sendPacket(ethSocket, (unsigned char*)packet.buf, packet.size_buf);
+            string_bintohex((unsigned char*)packet.string_bin, hex_packet);
+            printf("Data sent: %d bytes:\n%s\n", bytes_sended, &hex_packet[0]);
+        }
+        else if(selected_ptp_type == ptp_type::delay_resp_ptp){
+            unsigned char header[PTP_HEADER_SIZE * 8 + 1];
+            super_data receiveTimestamp;
+            super_data requestingPortIdentity;
+
+            generate_ptp_header(ptp_type::delay_resp_ptp, header);
+            
+            strcat(packet.string_bin, (char*)header);
+            packet.size_bin += strlen((char*)header);
+
+            cout << "Insert receiveTimestamp like 'abcdef0123456789abcd': " << endl;
+            cin >> receiveTimestamp.string_hex;
+            receiveTimestamp.size_hex = strlen(&receiveTimestamp.string_hex[0]);
+            string_hextobin(receiveTimestamp.string_hex, (unsigned char*)receiveTimestamp.string_bin, receiveTimestamp.size_hex * 8);
+            receiveTimestamp.size_bin = receiveTimestamp.size_hex * 4;
+            strcat(packet.string_bin, receiveTimestamp.string_bin);
+            packet.size_bin += receiveTimestamp.size_bin;
+
+            cout << "Insert requestingPortIdentity like 'abcdef0123456789abcd': " << endl;
+            cin >> requestingPortIdentity.string_hex;
+            requestingPortIdentity.size_hex = strlen(&requestingPortIdentity.string_hex[0]);
+            string_hextobin(requestingPortIdentity.string_hex, (unsigned char*)requestingPortIdentity.string_bin, requestingPortIdentity.size_hex * 8);
+            requestingPortIdentity.size_bin = requestingPortIdentity.size_hex * 4;
+            strcat(packet.string_bin, requestingPortIdentity.string_bin);
+            packet.size_bin += requestingPortIdentity.size_bin;
 
             packet_to_buf(packet.buf, packet.string_bin);
             packet.size_buf = packet.size_bin / 8;
