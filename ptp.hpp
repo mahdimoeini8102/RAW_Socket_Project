4b2e1c2d77a5d34acfd75caa25bc9b2f02863f34
mahdimoeini8102 <mahdimoeini8102@gmail.com>
2022-09-04 18:26:50 +0430

    "bit" changed to "flag"

--- ptp.hpp
+++ ptp.hpp
@@ -35,12 +35,12 @@ const string ptp_type_string[20] = {
 
 
 const string flag_field_string[17] = {
-                                "Insert leap61 (In PTP systems whose epoch is the PTP epoch, a TRUE value for this bit, shall indicate that the last minute of the current UTC day contains 61 seconds. If the epoch is not PTP, the value shall be set to FALSE.) ('0' or '1'): ",
-                                "Insert leap59 (In PTP systems whose epoch is the PTP epoch, a TRUE value for this bit, shall indicate that the last minute of the current UTC day contains 59 seconds. If the epoch is not PTP, the value shall be set to FALSE.) ('0' or '1'): ",
-                                "Insert currentUtcOffsetValid (The value of this bit, is TRUE if the timePropertiesDS.currentUtcOffset is known to be correct.) ('0' or '1'): ",
-                                "Insert ptpTimescale (The value of this bit is TRUE if the clock timescale of the grandmaster clock is PTP and FALSE otherwise.) ('0' or '1'): ",
-                                "Insert timeTraceable (The value of this bit, is TRUE if the timescale and the value of timePropertiesDS.currentUtcOffset are traceable to a primary reference; otherwise, the value shall be FALSE.) ('0' or '1'): ",
-                                "Insert frequencyTraceable (The value of this bit, is TRUE if the frequency determining the timescale is traceable to a primary reference; otherwise, the value shall be FALSE.) ('0' or '1'): ",
+                                "Insert leap61 (In PTP systems whose epoch is the PTP epoch, a TRUE value for this flag, shall indicate that the last minute of the current UTC day contains 61 seconds. If the epoch is not PTP, the value shall be set to FALSE.) ('0' or '1'): ",
+                                "Insert leap59 (In PTP systems whose epoch is the PTP epoch, a TRUE value for this flag, shall indicate that the last minute of the current UTC day contains 59 seconds. If the epoch is not PTP, the value shall be set to FALSE.) ('0' or '1'): ",
+                                "Insert currentUtcOffsetValid (The value of this flag, is TRUE if the timePropertiesDS.currentUtcOffset is known to be correct.) ('0' or '1'): ",
+                                "Insert ptpTimescale (The value of this flag is TRUE if the clock timescale of the grandmaster clock is PTP and FALSE otherwise.) ('0' or '1'): ",
+                                "Insert timeTraceable (The value of this flag, is TRUE if the timescale and the value of timePropertiesDS.currentUtcOffset are traceable to a primary reference; otherwise, the value shall be FALSE.) ('0' or '1'): ",
+                                "Insert frequencyTraceable (The value of this flag, is TRUE if the frequency determining the timescale is traceable to a primary reference; otherwise, the value shall be FALSE.) ('0' or '1'): ",
                                 "\0",
                                 "\0",
                                 "Insert alternateMasterFlag (A TRUE value for this flag indicates that the message is transmitted from a port not in the MASTER state.) ('0' or '1'): ",
